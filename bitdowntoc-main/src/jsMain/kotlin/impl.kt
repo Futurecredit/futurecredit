f08d9d00a41582ef7d86ed70910107501490847a
futurecredit <yanjinhong765@gmail.com>
2025-01-12 16:05:16 +0800

    Delete bitdowntoc-main directory

--- bitdowntoc-main/src/jsMain/kotlin/impl.kt
+++
@@ -1,6 +0,0 @@
-package ch.derlin.bitdowntoc
-
-actual fun String.removeDiacritics(): String =
-    this.normalize().replace("\\p{Diacritic}", "", ignoreCase = true)
-
-fun String.normalize(): String = asDynamic().normalize("NFD")
